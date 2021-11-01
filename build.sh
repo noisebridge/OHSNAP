aa7e5b25990f4ca1c79389f1e66571ed5b2f5745
Ramses A <iocoder@aol.com>
2021-10-31 23:20:59 -0400

    Add boot images and create initramfs

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# chdir into script directory
+cd `dirname $0`
+
 # create build directory
 rm -rf build
 mkdir build
@@ -10,9 +13,20 @@ touch flash.img
 truncate -s 32M flash.img
 
 # program the flash memory
-dd if=../boot/u-boot-spl.bin of=flash.img conv=notrunc oflag=seek_bytes seek=0M
-dd if=../boot/u-boot.bin     of=flash.img conv=notrunc oflag=seek_bytes seek=1M
-dd if=../boot/980uimage      of=flash.img conv=notrunc oflag=seek_bytes seek=2M
+dd if=../boot/u-boot-spl.bin of=flash.img conv=notrunc oflag=seek_bytes seek=0K     2>/dev/null
+dd if=../boot/u-boot-env.bin of=flash.img conv=notrunc oflag=seek_bytes seek=512K   2>/dev/null
+dd if=../boot/u-boot.bin     of=flash.img conv=notrunc oflag=seek_bytes seek=1024K  2>/dev/null
+dd if=../boot/980uimage      of=flash.img conv=notrunc oflag=seek_bytes seek=2048K  2>/dev/null
+dd if=../boot/uInitrd        of=flash.img conv=notrunc oflag=seek_bytes seek=16M    2>/dev/null
+
+# echo heading
+echo ''
+echo '+------------------------------------------------------------+'
+echo '|                                                            |'
+echo '|                OHSNAP QEMU-BASED EMULATOR                  |'
+echo '|                                                            |'
+echo '+------------------------------------------------------------+'
 
 # run QEMU
-/opt/qemu/bin/qemu-system-arm -s -M chili -bios flash.img -serial stdio
+/opt/qemu/bin/qemu-system-arm -s -M nuc980-soc -m 64M -bios flash.img -serial stdio -monitor none -nographic 2> qemu.log
+
