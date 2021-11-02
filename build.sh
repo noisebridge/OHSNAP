cec9d9746f7027e6c7d6c4efd1ab329d8ab9bb43
Ramses A <iocoder@aol.com>
2021-11-02 19:20:33 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -6,26 +6,27 @@ SRCDIR="$(cd `dirname $0`; pwd)"
 mkdir -p "${SRCDIR}"/build
 cd "${SRCDIR}"/build
 
-
 # compile qemu
 mkdir -p qemu
 cd qemu
-${SRCDIR}/external/qemu/configure --target-list=arm-softmmu --meson=${SRCDIR}/external/meson/meson.py
+if [ ! -f Makefile ]
+then
+  ${SRCDIR}/external/qemu/configure --target-list=arm-softmmu --meson=${SRCDIR}/external/meson/meson.py
+fi
 make -j20
 cd ..
 
-exit 0
-
-# create an empty flash memory image
+# create flash memory image
+mkdir -p images
+cd images
+rm -rf flash.img
 touch flash.img
 truncate -s 32M flash.img
-
-# program the flash memory
-dd if=../boot/u-boot-spl.bin of=flash.img conv=notrunc oflag=seek_bytes seek=0K     2>/dev/null
-dd if=../boot/u-boot-env.bin of=flash.img conv=notrunc oflag=seek_bytes seek=512K   2>/dev/null
-dd if=../boot/u-boot.bin     of=flash.img conv=notrunc oflag=seek_bytes seek=1024K  2>/dev/null
-dd if=../boot/980uimage      of=flash.img conv=notrunc oflag=seek_bytes seek=2048K  2>/dev/null
-dd if=../boot/uInitrd        of=flash.img conv=notrunc oflag=seek_bytes seek=16M    2>/dev/null
+dd if=${SRCDIR}/boot/u-boot-spl.bin of=flash.img conv=notrunc oflag=seek_bytes seek=0K     2>/dev/null
+dd if=${SRCDIR}/boot/u-boot-env.bin of=flash.img conv=notrunc oflag=seek_bytes seek=512K   2>/dev/null
+dd if=${SRCDIR}/boot/u-boot.bin     of=flash.img conv=notrunc oflag=seek_bytes seek=1024K  2>/dev/null
+dd if=${SRCDIR}/boot/980uimage      of=flash.img conv=notrunc oflag=seek_bytes seek=2048K  2>/dev/null
+cd ..
 
 # echo heading
 echo ''
@@ -36,5 +37,5 @@ echo '|                                                            |'
 echo '+------------------------------------------------------------+'
 
 # run QEMU
-/opt/qemu/bin/qemu-system-arm -s -M nuc980-soc -m 64M -bios flash.img -serial stdio -monitor none -nographic
+qemu/qemu-system-arm -s -M nuc980-soc -m 64M -bios flash.img -serial stdio -monitor none -nographic
 
